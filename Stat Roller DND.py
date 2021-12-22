1a55a3e7864fd4c64087a574961139e132123921
kaitlynmichaud <90233824+kaitlynmichaud@users.noreply.github.com>
2021-12-22 16:24:09 -0600

    Add files via upload
    Based off of rolling stats in Dungeons and Dragons, the user is asked how many die would they like to roll. Then, the die is rolled however many times they want and the lowest number is dropped.

---
+++ Stat Roller DND.py
@@ -0,0 +1,19 @@
+import random
+import time
+
+num_die = input('How many die would you like to roll? ')
+
+results = [] 
+n = int(num_die)
+print('Rolling the dice...')
+
+for i in range(n): # rolls dice n times
+    dice = random.randint(1,100)
+    results.append(dice) #add each roll to result list
+
+
+results.sort()
+results.pop(0)
+print("Your results with the lowest number taken out are: ", results)
+    
+
