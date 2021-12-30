5417004db7b8192d11578996bd149856dc63ce4f
Kaitlyn Michaud <kaitlynmichaud2001@gmail.com>
2021-12-30 01:55:39 -0600

    Add files via upload

---
+++ Calculator Program.py
@@ -0,0 +1,10 @@
+# GUI calculator using Tkinter
+
+from tkinter import *
+expression = ""
+
+window = Tk()
+window.title('Python GUI App')
+window.configure(width = 500, height = 300)
+window.configure(bg='pink')
+window.mainloop()
